12db72e25161c1ad431dac6478262c8c5031e4eb
Pete Karl II <pete.karl@gmail.com>
2023-09-17 10:22:26 -0400

    updated setup and related info with simplified package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gchat-card-builder",
+    name="gchatcardbuilder",
     version="0.1",
     packages=find_packages(),
     install_requires=[
@@ -13,17 +13,14 @@ setup(
     description="A clean, type-safe interface for generating rich chat responses to the Google Chat API using the CardsV2 schema.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/pkarl/gchat-card-builder",  # Add your repo URL here
+    url="https://github.com/pkarl/gchatcardbuilder",  # Add your repo URL here
     classifiers=[
-        "Development Status :: 3 - Alpha",  # or '5 - Production/Stable' if it's stable
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="google chat api, cardsv2, gchat, chatbot, chat bot, google chat",
 )
