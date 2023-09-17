db340744a95bb4820a7521e9b47b6db297d31e5a
Pete Karl II <pete.karl@gmail.com>
2023-09-17 10:30:13 -0400

    formatting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gchatcardbuilder",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         # List dependencies here, for example:
